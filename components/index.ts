cfed8f06a6f1816249d6e1a7dae1458d39434e07
beboopr <linnets-router.09@icloud.com>
2023-09-26 03:21:40 -0400

    imported some components

--- components/index.ts
+++ components/index.ts
@@ -1,19 +1,21 @@
 // import CarCard from "./CarCard";
 import CustomButton from "./CustomButton";
-// import CustomFilter from "./CustomFilter";
+import CustomFilter from "./CustomFilter";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 // import ShowMore from "./ShowMore";
-// import SearchBar from "./Searchbar";
+import SearchBar from "./SearchBar";
 import Hero from "./Hero";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
 Hero,
 //   CarCard,
   CustomButton,
-//   CustomFilter,
+  CustomFilter,
   Footer,
   NavBar,
 //   ShowMore,
-//   SearchBar,
+  SearchBar,
+  SearchManufacturer,
 };
